3ccc0364582dd7052241fe6ec56d4db66d4196ee
Jason Cooper <jason@codecooper.com>
2022-07-25 08:34:44 -0600

    Coverage 79.7% `AdditionalProperties` are only used to capture items that are not mapped correctly. Therefore there is no reason to update them. This tests finds all `AdditionalProperties` properties and instantiates them. Simple, but raised the method coverage significantly.

--- tests/Ocelli.OpenShopify.Tests/MisscellaneousTests.cs
+++ tests/Ocelli.OpenShopify.Tests/MisscellaneousTests.cs
@@ -1,22 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Ocelli.OpenShopify.Tests;
 public class MiscellaneousTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public MiscellaneousTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     [Fact]
     public void AdditionalProperties_CanUpdate()
     {
-        var types = Assembly
-            .GetExecutingAssembly()
-            .GetTypes()
-            .Where(t => t.Namespace.StartsWith("Ocelli.OpenShopify"));
-
-        foreach (var t in types)
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => a.FullName != null && a.FullName.StartsWith("Ocelli.OpenShopify") && !a.FullName.Contains("Test"));
+        foreach (var assembly in assemblies)
         {
-            Console.WriteLine(t.Name);
+            foreach (var type in assembly.GetTypes())
+            {
+                var properties = type.GetProperties().Where(p => p.Name == nameof(AccessScope.AdditionalProperties));
+                foreach (var property in properties)
+                {
+                    _testOutputHelper.WriteLine($@"{type.Name}.{property.Name}");
+                    var c = Activator.CreateInstance(type);
+                    c?.GetType().GetProperty(property.Name)?.SetValue(c, null, null);
+                }
+
+            }
         }
     }
 }
